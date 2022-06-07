aad1862b4856a70ef571cefdb313fea9e4e822cf
DaYeong96 <102666062+DaYeong96@users.noreply.github.com>
2022-06-07 17:34:57 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -22,16 +22,21 @@ def welcome():
 
 #################################
 class INSP_def:
-    st.title('용해탱크 이상탐지 예측 서비스')
-    st.title(' ')
-    st.subheader('(1) 생산품의 수분함유량 10개 입력해주세요.')
-    st.subheader('시간의 흐름을 판단하기 위해 10개의 값이 필요합니다.')
-    st.title(' ')
+#     st.title('용해탱크 이상탐지 예측 서비스')
+#     st.title(' ')
+#     st.subheader('(1) 생산품의 수분함유량 10개 입력해주세요.')
+#     st.subheader('시간의 흐름을 판단하기 위해 10개의 값이 필요합니다.')
+#     st.title(' ')
 
     
     
     #def __init__(self, INSP1, INSP2, INSP3, INSP4, INSP5, INSP6, INSP7,INSP8, INSP9, INSP10):  
     def __init__(self):
+        st.title('용해탱크 이상탐지 예측 서비스')
+        st.title(' ')
+        st.subheader('(1) 생산품의 수분함유량 10개 입력해주세요.')
+        st.subheader('시간의 흐름을 판단하기 위해 10개의 값이 필요합니다.')
+        st.title(' ')
         
         self.INSP1 = st.slider('1 생산품의 수분함유량을 입력하세요', 0, 5)    #변수별로 10개씩 
         #st.write('1 생산품의 수분함유량:', INSP1)
